f978c658145806ad1cbcb703a2cc4c7ca3a56fb4
Paul Feitzinger <paul.feitzinger@gmail.com>
2012-07-17 19:08:56 -0400

    move basic docs to README

---
+++ README.txt
@@ -0,0 +1,7 @@
+Converts a CHILDES XML file to a plain-text larc-tagged slash/tag
+format. Writes to standard out.
+
+usage:
+
+  python xml2slashtags.py adam01.xml adam02.xml > adam-1-2-slashtags.txt
+
--- xml2slashtags.py
+++ xml2slashtags.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python2
 
-""" Converts a CHILDES XML file to a plain-text larc-tagged slash/tag
-format. Writes to standard out.
-
-usage:
-
-  python xml2slashtags.py adam01.xml adam02.xml > adam-1-2-slashtags.txt
-
-"""
-
 import csv
 import re
 import sys
